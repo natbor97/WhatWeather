065f8fd8ed2649596c513583dfcb958960e7fc84
Natalia Borkowska <natalia.borkowska.programista@gmail.com>
2023-03-18 14:25:03 +0100

    weather display view

---
+++ src/main/java/pl/nataliamichalowska/controller/WeatherDisplayController.java
@@ -0,0 +1,57 @@
+package pl.nataliamichalowska.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class WeatherDisplayController {
+    @FXML
+    private Label dateToday1;
+
+    @FXML
+    private Label dateToday2;
+
+    @FXML
+    private Label descriptionMyCityToday;
+
+    @FXML
+    private Label descriptionPurposeCityToday;
+
+    @FXML
+    private Group groupInitialToday1;
+
+    @FXML
+    private Group groupInitialToday11;
+
+    @FXML
+    private ImageView imageMyCityToday;
+
+    @FXML
+    private ImageView imagePurposeCityToday;
+
+    @FXML
+    private VBox initialForecastBox;
+
+    @FXML
+    private VBox initialForecastBox1;
+
+    @FXML
+    private Label myCity;
+
+    @FXML
+    private Label pressureMyCityToday;
+
+    @FXML
+    private Label pressurePurposeCityToday;
+
+    @FXML
+    private Label purposeCity;
+
+    @FXML
+    private Label tempMyCityToday;
+
+    @FXML
+    private Label tempPurposeCityToday;
+}
